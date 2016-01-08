e389498a33c33457331b5324719e23479b30baf8
jtmelton <jtmelton@gmail.com>
2016-01-07 22:54:58 -0500

    updating block refresh

--- blocks/refresh.go
+++ blocks/refresh.go
@@ -19,25 +19,25 @@ func RefreshBlocks(blockRefreshUrl *string) {
 	}
 
 	for _, element := range blocks {
-		
+
 		// temporary hack for dealing w/ java block store
 		// it keeps timestamp in microseconds for some reason
 		element.Endtime = element.Endtime / 1000
-		
+
 		t, _ := unixToTime(element.Endtime)
-		
+
 		// only add if this is still a valid time block
 		if time.Now().Before(t) {
 			//back to json so it's hashable
 			jsonStr, _ := json.Marshal(element)
 			StoredBlocks.Add(string(jsonStr))
 		}
-			
+
 	}
 
-//	glog.Info(StoredBlocks.Len())
+	//	glog.Info(StoredBlocks.Len())
 	glog.Infof("Retrieved %d blocks, total stored: %d", len(blocks), StoredBlocks.Len())
-//	glog.Info("STORED blocks: ", StoredBlocks.Flatten())
-//	glog.Info("Printing response: ", resp)
-//	glog.Info("Printing body: ", body)
+	//	glog.Info("STORED blocks: ", StoredBlocks.Flatten())
+	//	glog.Info("Printing response: ", resp)
+	//	glog.Info("Printing body: ", body)
 }
